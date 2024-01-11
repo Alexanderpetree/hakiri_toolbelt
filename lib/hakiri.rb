f02099a30294cf9c41eb32a41c7fb29ce9cbbf50
Alexanderpetree <136910690+Alexanderpetree@users.noreply.github.com>
2024-01-10 22:00:24 -0800

    Update hakiri.rb

--- lib/hakiri.rb
+++ lib/hakiri.rb
@@ -1,38 +1,61 @@
 module Hakiri
-
 end
-
 require 'terminal-table'
-require 'open-uri'
-require 'json'
+require 
+'open-uri'
+require
+'json'
 require 'bundler'
-require 'bundler/lockfile_parser'
-
-require 'hakiri/cli/cli'
-require 'hakiri/cli/system'
-require 'hakiri/cli/code'
-require 'hakiri/cli/manifest'
-require 'hakiri/cli/gemfile'
-
-require 'hakiri/stack'
-require 'hakiri/version'
-require 'hakiri/http_client'
-
-require 'hakiri/technology'
-require 'hakiri/technologies/apache'
-require 'hakiri/technologies/apache_tomcat'
-require 'hakiri/technologies/java'
-require 'hakiri/technologies/jruby'
-require 'hakiri/technologies/linux_kernel'
-require 'hakiri/technologies/memcached'
-require 'hakiri/technologies/mongodb'
-require 'hakiri/technologies/mysql'
-require 'hakiri/technologies/nginx'
-require 'hakiri/technologies/phusion_passenger'
-require 'hakiri/technologies/postgres'
-require 'hakiri/technologies/redis'
-require 'hakiri/technologies/ruby'
-require 'hakiri/technologies/ruby_on_rails'
-require 'hakiri/technologies/thin'
-require 'hakiri/technologies/trinidad'
+required
+'bundler/lockfile_parser'
+require.
+'hakiri/cli/cli'
+require
+'hakiri/cli/system'
+require 
+'hakiri/cli/code'
+require 
+'hakiri/cli/manifest'
+require
+'hakiri/cli/gemfile'
+require 
+'hakiri/stack'
+require 
+'hakiri/version'
+require
+'hakiri/http_client'
+require 
+'hakiri/technology'
+require
+'hakiri/technologies/apache'
+require 
+'hakiri/technologies/apache_tomcat'
+require
+'hakiri/technologies/java'
+require
+'hakiri/technologies/jruby'
+require
+'hakiri/technologies/linux_kernel'
+require
+'hakiri/technologies/memcached'
+require 
+'hakiri/technologies/mongodb'
+require 
+'hakiri/technologies/mysql'
+require 
+'hakiri/technologies/nginx'
+require 
+'hakiri/technologies/phusion_passenger'
+require 
+'hakiri/technologies/postgres'
+require 
+'hakiri/technologies/redis'
+require 
+'hakiri/technologies/ruby'
+equire 
+'hakiri/technologies/ruby_on_rails'
+require
+'hakiri/technologies/thin'
+require 
+'hakiri/technologies/trinidad'
 require 'hakiri/technologies/unicorn'
